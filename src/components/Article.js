785ee20e64c5710661535e850dcb8b1e4605ae9b
Roman Iakobchuk <r.iakobchuk@gmail.com>
2017-09-29 20:51:40 +0300

    add reverse dataflow

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,32 +1,30 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 
 class Article extends Component {
-    state = {
-        isOpen: true
+    static propTypes = {
+        article: PropTypes.shape({
+            title: PropTypes.string.isRequired,
+            text: PropTypes.string,
+            date: PropTypes.string.isRequired
+        }).isRequired
     }
 
     render() {
-        const {article} = this.props
-        const body = this.state.isOpen && <section>{article.text}</section>
+        const {article, isOpen, onButtonClick} = this.props
+        const body = isOpen && <section>{article.text}</section>
         return (
             <div>
                 <h2>
                     {article.title}
-                    <button onClick={this.handleClick}>
-                        {this.state.isOpen ? 'close' : 'open'}
+                    <button onClick={onButtonClick}>
+                        {isOpen ? 'close' : 'open'}
                     </button>
                 </h2>
                 {body}
                 <h3>creation date: {(new Date(article.date)).toDateString()}</h3>
             </div>
         )
-    }
-
-    handleClick = () => {
-        console.log('---', 'clicked')
-        this.setState({
-            isOpen: !this.state.isOpen
-        })
     }
 }
 
