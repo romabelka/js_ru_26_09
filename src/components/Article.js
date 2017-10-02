b05ffb804f4a2807a7be665b9c063ecb58922403
Oleg <helgi.mail.box@gmail.com>
2017-10-02 22:06:37 +0300

    'HT1.1

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,18 +1,37 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
+import Comment from './Comment'
 
 class Article extends Component {
     static propTypes = {
         article: PropTypes.shape({
             title: PropTypes.string.isRequired,
             text: PropTypes.string,
-            date: PropTypes.string.isRequired
+            date: PropTypes.string.isRequired,
+            comments: PropTypes.array
         }).isRequired
     }
 
+    state = {
+        openComments: false
+    }
+
+    onCommentsButtonClick = () => this.setState({ openComments: !this.state.openComments })
+
     render() {
+
         const {article, isOpen, onButtonClick} = this.props
         const body = isOpen && <section>{article.text}</section>
+        const comments = article.comments ?
+            article.comments.map(comment => (
+                <Comment
+                    key={comment.id}
+                    user={comment.user}
+                    text={comment.text}
+                />
+            )) :
+            <h3>No comments</h3>
+        const commentsList = this.state.openComments && <ul>{comments}</ul>
         return (
             <div>
                 <h2>
@@ -23,6 +42,10 @@ class Article extends Component {
                 </h2>
                 {body}
                 <h3>creation date: {(new Date(article.date)).toDateString()}</h3>
+                <button onClick={this.onCommentsButtonClick}>
+                    {this.state.openComments ? 'close comments' : 'open comments'}
+                </button>
+                {commentsList}
             </div>
         )
     }
