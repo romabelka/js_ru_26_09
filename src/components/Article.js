5951e47a56e7be1e86bbe990b2a41488e2895468
aleksandra-maslennikova <alex.maslennikova19@gmail.com>
2017-10-02 10:20:01 +0300

    added comments list

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,5 +1,6 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import CommentsList from './CommentsList'
 
 class Article extends Component {
     static propTypes = {
@@ -10,8 +11,15 @@ class Article extends Component {
         }).isRequired
     }
 
+    state = {
+        commentsAreOpened: false
+    }
+    openComments = () => {
+        this.setState({ commentsAreOpened: !this.state.commentsAreOpened })
+    }
+
     render() {
-        const {article, isOpen, onButtonClick} = this.props
+        const { article, isOpen, onButtonClick } = this.props
         const body = isOpen && <section>{article.text}</section>
         return (
             <div>
@@ -23,6 +31,11 @@ class Article extends Component {
                 </h2>
                 {body}
                 <h3>creation date: {(new Date(article.date)).toDateString()}</h3>
+                {isOpen &&
+                    <div>
+                        <button onClick={this.openComments}> {this.state.commentsAreOpened ? 'close comments' : 'open comments'}</button>
+                        {this.state.commentsAreOpened && <CommentsList comments={article.comments} />}
+                    </div>}
             </div>
         )
     }
