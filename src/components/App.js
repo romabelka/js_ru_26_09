03c0b6faf823fcdd0dd0e520d3dde543bc113035
Roman Iakobchuk <r.iakobchuk@gmail.com>
2017-10-06 20:20:46 +0300

    add UserForm

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ArticleList from './ArticleList'
 import ArticlesChart from './ArticlesChart'
+import UserForm from './UserForm'
 import articles from '../fixtures'
 
-function App() {
-    return (
-        <div>
-            <h1>App name</h1>
-            <ArticleList articles = {articles} />
-            <ArticlesChart articles = {articles} />
-        </div>
-    )
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>App name</h1>
+                <UserForm />
+                <ArticleList articles={articles}/>
+                <ArticlesChart articles={articles}/>
+            </div>
+        )
+    }
 }
 
 App.propTypes = {
