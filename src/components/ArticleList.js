fb559e3e5ab65feced1b33e7026132d942023ab6
Roman Iakobchuk <r.iakobchuk@gmail.com>
2017-10-06 17:05:12 +0300

    add HW2

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,60 +1,29 @@
-import React, { Component } from 'react'
-import Article from './Article'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
+import Article from './Article'
+import Accordion from './common/Accordion'
 
-class ArticleList extends Component {
-    static defaultProps = {
-        articles: [],
-    }
-
-    state = {
-        openArticleId: null
-    }
-
-    componentWillMount() {
-        console.log('---', 'mounting')
-    }
-
-    componentDidMount() {
-        console.log('---', 'mounted')
-    }
-
-    componentWillUpdate(_, nextState) {
-        console.log('---', nextState.openArticleId)
-    }
-
+class ArticleList extends Accordion {
     render() {
-        if (!this.props.articles.length) return <h3>No articles</h3>
-
-        const articleElements = this.props.articles.map(article => (
-            <li key = {article.id}>
-                <Article
-                    article = {article}
-                    isOpen = {this.state.openArticleId === article.id}
-                    onButtonClick = {this.toggleOpenArticle(article.id)}
-                />
-            </li>
-        ))
+        const {articles} = this.props
+        if (!articles.length) return <h3>No Articles</h3>
+        const articleElements = articles.map((article) => <li key={article.id}>
+            <Article article={article}
+                     isOpen={article.id === this.state.openItemId}
+                     onButtonClick={this.toggleOpenItemMemoized(article.id)}
+            />
+        </li>)
         return (
             <ul>
                 {articleElements}
             </ul>
         )
     }
+}
 
-    toggleOpenArticle = (id) => {
-        if (this.memoizedTogglers.get(id)) return this.memoizedTogglers.get(id)
 
-        const func = (ev) => this.setState({
-            openArticleId: id === this.state.openArticleId ? null : id
-        })
-
-        this.memoizedTogglers.set(id, func)
-
-        return func
-    }
-
-    memoizedTogglers = new Map()
+ArticleList.defaultProps = {
+    articles: []
 }
 
 ArticleList.propTypes = {
