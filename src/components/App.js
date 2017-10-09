57a14a293183fcb82d8a9044400057c02b33fbfe
KatrinaBosh <katrinabosh@gmail.ru>
2017-10-09 18:36:07 +0400

    ##HT3.1 Добавить на уровне App календарь(https://github.com/gpbl/react-day-picker) с выбором диапазона дат, отображать этот диапазон текстом

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,18 @@
 import React, {Component} from 'react'
 import Select from 'react-select'
+import DayPicker, {DateUtils} from 'react-day-picker'
 import ArticleList from './ArticleList'
 import ArticlesChart from './ArticlesChart'
 import UserForm from './UserForm'
 import articles from '../fixtures'
 import 'react-select/dist/react-select.css'
+import 'react-day-picker/lib/style.css'
 
 class App extends Component {
     state = {
-        selected: null
+        selected: null,
+        fromDay: null,
+        toDay: null
     }
 
     render() {
@@ -16,11 +20,24 @@ class App extends Component {
             label: article.title,
             value: article.id
         }))
+        const {fromDay, toDay} = {fromDay: this.state.fromDay, toDay: this.state.toDay}
+
+        const periodElements = <div>
+            <DayPicker  numberOfMonths={2}
+                        selectedDays={[fromDay, { fromDay, toDay }]}
+                        onDayClick={this.handleDayClick}
+                        fixedWeeks
+            />
+            <h4>Selected range:</h4>
+            <h4>Начальная дата {fromDay ? fromDay.toLocaleString() : 'не задана'} </h4>
+            <h4>Конечная дата {toDay ? toDay.toLocaleString() : 'не задана'} </h4>
+        </div>
         return (
             <div>
                 <h1>App name</h1>
                 <UserForm />
                 <Select options = {options} value = {this.state.selected} onChange = {this.handleChange} multi/>
+                {periodElements}
                 <ArticleList articles = {articles}/>
                 <ArticlesChart articles = {articles}/>
             </div>
@@ -28,6 +45,10 @@ class App extends Component {
     }
 
     handleChange = selected => this.setState({ selected })
+    handleDayClick = day => {
+        const range = DateUtils.addDayToRange(day, {from: this.state.fromDay, to: this.state.toDay});
+        this.setState({fromDay: range.from, toDay: range.to});
+    };
 }
 
 App.propTypes = {
