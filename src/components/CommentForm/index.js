75c2de0cdd5ed95537dd9c03940bde28f2392723
Yurii Matsko <yuriimatsko@Movinga-35-1384.local>
2017-10-09 17:16:24 +0200

    HT3 add CommentForm folder and styles

--- src/components/CommentForm/index.js
+++ src/components/CommentForm/index.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 
+import './CommentForm.css'
+
 class CommentForm extends Component {
     state = {
         user: '',
-        comment: '',
-        canSubmit: false
+        comment: ''
     }
 
     render() {
@@ -17,10 +18,10 @@ class CommentForm extends Component {
                     <input type='text' name='user' maxLength='50' placeholder='Name' value={user} className={this.inputClassName('user')} onChange={this.handleChange} />
                 </div>
                 <div>
-                    <textarea name='comment' maxLength='50' placeholder='Write your comment here' value={comment} onChange={this.handleChange} />
+                    <textarea name='comment' maxLength='50' placeholder='Write your comment here' value={comment} className={this.inputClassName('comment')} onChange={this.handleChange} />
                 </div>
                 <div>
-                    <input type='submit' value='Submit' disabled={this.canSubmit} />
+                    <input type='submit' value='Submit' disabled={!this.canSubmit()} />
                 </div>
             </form>
         )
@@ -36,15 +37,19 @@ class CommentForm extends Component {
         })
     }
 
-    inputClassName = (e) => this.isInvalid(e) ? 'invalid' : ''
-
     isInvalid = (e) => this.state[e].length < 10
 
+    inputClassName = (e) => this.isInvalid(e) ? 'invalid' : ''
+
+    canSubmit = (e) => !this.isInvalid('user') && !this.isInvalid('comment')
+
     handleSubmit = (e) => {
-        e.preventDefault();
+        e.preventDefault()
 
-
-        console.log(this.state);
+        this.setState({
+            user: '',
+            comment: ''
+        })
     }
 }
 
