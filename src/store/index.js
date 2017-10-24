488111630d29cb969b067d30c4294464a2e8459a
Roman Iakobchuk <kaboom.ua@gmail.com>
2017-10-24 20:55:46 +0300

    add redux-devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,18 @@
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import reducer from '../reducer'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 import randomId from '../middlewares/randomId'
 import api from '../middlewares/api'
 
-const enhancer = applyMiddleware(thunk, randomId, api, logger)
+const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        }) : compose
+
+const enhancer = composeEnhancers(applyMiddleware(thunk, randomId, api, logger))
 
 const store = createStore(reducer, enhancer)
 
