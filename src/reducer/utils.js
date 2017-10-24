e94946f0bca13bce5db4499a7bc61a4875dc18b0
Roman Iakobchuk <kaboom.ua@gmail.com>
2017-10-24 20:07:00 +0300

    add immutable

--- src/reducer/utils.js
+++ src/reducer/utils.js
@@ -1,6 +1,5 @@
-export function arrToMap(arr) {
-    return arr.reduce((acc, item) => ({
-        ...acc,
-        [item.id]: item
-    }), {})
+import {Map} from 'immutable'
+
+export function arrToMap(arr, DataRecord) {
+    return arr.reduce((acc, item) => acc.set(item.id, DataRecord ? new DataRecord(item) : item), new Map({}))
 }
--- src/selectors/index.js
+++ src/selectors/index.js
@@ -5,7 +5,7 @@ export const articlesMapSelector = state => state.articles
 export const commentsSelector = state => state.comments
 export const idSelector = (_, props) => props.id
 
-export const articlesSelector = createSelector(articlesMapSelector, articles => Object.values(articles))
+export const articlesSelector = createSelector(articlesMapSelector, articles => articles.valueSeq().toArray())
 
 export const filteredArticlesSelector = createSelector(articlesSelector, filtersSelector, (articles, filters) => {
     const {selected, dateRange: {from, to}} = filters
@@ -21,6 +21,6 @@ export const filteredArticlesSelector = createSelector(articlesSelector, filters
 export const createCommentSelector = () => {
     return createSelector(commentsSelector, idSelector, (comments, id) => {
         console.log('---', 'comment selector', id)
-        return comments[id]
+        return comments.get(id)
     })
 }
