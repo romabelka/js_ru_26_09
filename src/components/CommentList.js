c95a424bf5e28cd9a615d76364d4086ace1b5215
Nikita <almonah@gmail.com>
2017-10-26 19:11:54 +0300

    HT6 done

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -2,13 +2,20 @@ import React, {Component} from 'react'
 import Comment from './Comment'
 import PropTypes from 'prop-types'
 import toggleOpen from '../decorators/toggleOpen'
+import {loadCommentsByArticleId} from '../AC'
 import CommentForm from './CommentForm'
+import {connect} from 'react-redux'
+import Loader from './common/Loader'
 
 class CommentList extends Component {
     static defaultProps = {
         article: PropTypes.object.isRequired,
         isOpen: PropTypes.bool,
         toggleOpen: PropTypes.func
+    }
+
+    componentWillReceiveProps({ isOpen, article, loadCommentsByArticleId }) {
+        if (isOpen && !this.props.isOpen && !article.commentsLoaded) loadCommentsByArticleId(article.id)
     }
 
     render() {
@@ -23,8 +30,9 @@ class CommentList extends Component {
     }
 
     getBody() {
-        const { article: {id, comments = []}, isOpen } = this.props
+        const { article: {id, commentsLoaded, comments = []}, isOpen } = this.props
         if (!isOpen) return null
+        if (!commentsLoaded) return <Loader/>
 
         const body = comments.length ? (
             <ul>
@@ -42,4 +50,4 @@ class CommentList extends Component {
 }
 
 
-export default toggleOpen(CommentList)
+export default connect(null, {loadCommentsByArticleId})(toggleOpen(CommentList))
--- src/reducer/comments.js
+++ src/reducer/comments.js
@@ -1,16 +1,37 @@
-import { ADD_COMMENT } from '../constants'
+import { ADD_COMMENT, LOAD_ARTICLE_COMMENTS, START, SUCCESS } from '../constants'
 import {normalizedComments} from '../fixtures'
 import {arrToMap} from './utils'
+import {Record, Map} from 'immutable'
 
-export default (state = arrToMap(normalizedComments), action) => {
+const ReducerRecord = Record({
+    entities: new Map({}),
+    loading: false,
+    loaded: false
+})
+
+const CommentRecord = Record({
+    id: null,
+    user: null,
+    text: null
+})
+
+export default (state = new ReducerRecord, action) => {
     const { type, payload, randomId } = action
 
     switch (type) {
         case ADD_COMMENT:
-            return {...state, [randomId]: {
-                ...payload.comment,
-                id: randomId
-            }}
+            return state
+                .setIn(['entities', randomId], new CommentRecord(payload.comment))
+
+        case LOAD_ARTICLE_COMMENTS + START:
+            return state.set('loading', true)
+
+        case LOAD_ARTICLE_COMMENTS + SUCCESS:
+            console.log("LOAD_ARTICLE_COMMENTS:::", state)
+            return state
+                .set('loading', false)
+                .set('loaded', true)
+                .set('entities', state.entities.merge(arrToMap(payload.response, CommentRecord)))
     }
 
     return state
